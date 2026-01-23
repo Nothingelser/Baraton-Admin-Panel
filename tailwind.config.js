8711f620882f0b44f7d615df172b44943bd435df
Nothingelser <jayish6035@gmail.com>
2026-01-23 17:46:05 +0300

    Initial commit - fresh start

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  darkMode: 'class', // CHANGE THIS LINE
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
